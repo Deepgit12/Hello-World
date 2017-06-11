729f6a70cee0f46e7cdbb8f151af601dbfc0cc8b
Deepgit12 <deepak.dk10@gmail.com>
2017-06-11 14:53:49 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		A.sayhi();
+		
+
+	}
+}
+	class A
+	{
+		static void sayhi()
+		{
+		System.out.println("Welcome to Class B");
+	}
+
+}
